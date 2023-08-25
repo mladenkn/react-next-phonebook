9ccb41bf6aa1401c991133eebd9c4987c6216de0
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-08-25 16:31:17 +0200

    updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig, Plugin } from "vite"
-import { resolve } from "path"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 import svgrPlugin from "vite-plugin-svgr"
 import checker from "vite-plugin-checker"
-// import handlebars from "vite-plugin-handlebars"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +17,6 @@ export default defineConfig({
     }),
     viteTsconfigPaths(),
     svgrPlugin(),
-    // handlebars({
-    //   partialDirectory: resolve(__dirname, "src/partials"),
-    // }) as Plugin,
   ],
   server: {
     port: 3000,
