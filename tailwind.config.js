9bcdb01cc119e78ad716b8e36562c8750866fdde
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-02 20:09:35 +0100

    tailwind +

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: {
-        light: "#78C9CE",
-        main: "#2DA1AD",
-        dark: "#2496A2",
-      },
-      secondary: {
-        main: "#BBC4C3",
-        light: "#E3E3E3",
-      },
-      tc: {
-        primary: colors.grey[500],
-        secondary: "#2DA1AD",
-      },
-    }
+    extend: {
+      colors: {
+        primary: {
+          light: "#78C9CE",
+          main: "#2DA1AD",
+          dark: "#2496A2",
+        },
+        secondary: {
+          main: "#BBC4C3",
+          light: "#E3E3E3",
+        },
+        tc: {
+          primary: colors.grey[500],
+          secondary: "#2DA1AD",
+        },
+      }
+    },
   },
   plugins: [],
 }
