0325b03769f1c42a7e8c1c57057a245fd9497079
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-04 21:05:09 +0100

    tailwind +

--- src/phonebook/components/ContactList/ContactListItem.tsx
+++ src/phonebook/components/ContactList/ContactListItem.tsx
@@ -61,45 +61,44 @@ const _ContactListItem = ({
     <DeleteAction onConfirm={onDelete} iconClass={classes.icon} />
   )
 
-  if (width === "md" && !isLinkToDetails) {
-    return (
-      <div
-        className="flex h-full flex-col items-center justify-center border-2 border-solid border-secondary-light"
-        onClick={onSelect}
-      >
-        {avatar}
-        {name}
-      </div>
-    )
-  }
-
-  if (width === "md" && isLinkToDetails) {
-    return (
-      <Link className="flex h-full flex-col items-center justify-center border-2 border-solid border-primary-main">
-        <div className="flex w-full justify-between">
-          {favoriteAction}
+  switch (true) {
+    case width === "md" && !isLinkToDetails:
+      return (
+        <div
+          className="flex h-full flex-col items-center justify-center border-2 border-solid border-secondary-light"
+          onClick={onSelect}
+        >
+          {avatar}
+          {name}
+        </div>
+      )
+    case width === "md" && isLinkToDetails:
+      return (
+        <Link className="flex h-full flex-col items-center justify-center border-2 border-solid border-primary-main">
+          <div className="flex w-full justify-between px-1.5">
+            {favoriteAction}
+            <span>
+              {editAction}
+              {deleteAction}
+            </span>
+          </div>
+          {avatar}
+          {name}
+        </Link>
+      )
+    default:
+      return (
+        <div className="flex h-full w-full items-center justify-between border-2 border-solid border-secondary-light shadow-none">
+          {avatar}
+          {name}
           <span>
+            {favoriteAction}
             {editAction}
             {deleteAction}
           </span>
         </div>
-        {avatar}
-        {name}
-      </Link>
-    )
+      )
   }
-
-  return (
-    <div className="flex h-full w-full items-center justify-between border-2 border-solid border-secondary-light shadow-none">
-      {avatar}
-      {name}
-      <span>
-        {favoriteAction}
-        {editAction}
-        {deleteAction}
-      </span>
-    </div>
-  )
 }
 
 export const ContactListItem = withWidth()(withStyles(style)(_ContactListItem))
