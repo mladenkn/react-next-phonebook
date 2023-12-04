df5d022fc0f3a1eadab4fa777549355658349d77
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-04 20:42:31 +0100

    tailwind +

--- src/phonebook/components/ContactList/ContactListItem.tsx
+++ src/phonebook/components/ContactList/ContactListItem.tsx
@@ -1,11 +1,8 @@
 import style from "./ContactListItem.style"
 import { ContactListItem as ContactListItemModel } from "../../models"
-import { Paper, useTheme, withStyles, WithStyles } from "@material-ui/core"
-import { Link } from "../various"
-import { contactDetailsUrl } from "../../urls"
+import { withStyles, WithStyles } from "@material-ui/core"
 import { GoToEditAction, FavoriteAction, DeleteAction } from "../actions"
 import { ContactAvatar } from "../ContactAvatar"
-import clsx from "clsx"
 
 type ItemPresenterProps = {
   contact: ContactListItemModel
@@ -29,68 +26,49 @@ const _ContactListItem = ({
   const showDeleteButton = showEditLink
   const isLinkToDetails = showEditLink
 
-  const avatarAndName = (
-    <div
-      className={clsx(
-        "flex h-full w-full items-center border-2 border-solid border-secondary-light shadow-none md:block",
-        classes.avatarAndName,
-        isSelected && "border-primary-main",
-      )}
-    >
-      <ContactAvatar
-        className="md:order-2 md:m-auto md:mb-2 md:h-12 md:w-12"
-        letter={contact.fullName[0]}
-        style={contact.avatar}
-        url={contact.avatarUrl}
-      />
-      <div className={classes.nameBox}>
-        <p style={{ color: "rgba(0, 0, 0, 0.54)" }} className="font-sans text-sm lg:text-center">
-          {contact.fullName}
-        </p>
-      </div>
-    </div>
+  const avatar = (
+    <ContactAvatar
+      className="md:m-auto md:mb-2 md:h-12 md:w-12"
+      letter={contact.fullName[0]}
+      style={contact.avatar}
+      url={contact.avatarUrl}
+    />
+  )
+
+  const name = (
+    <p style={{ color: "rgba(0, 0, 0, 0.54)" }} className="font-sans text-sm lg:text-center">
+      {contact.fullName}
+    </p>
   )
 
   const favoriteAction = (
     <FavoriteAction
       onClick={onToggleFavorite}
       isFavorite={contact.isFavorite}
-      rootClass={clsx(classes.action, classes.favoriteAction)}
       iconClass={classes.icon}
     />
   )
 
   const editAction = showEditLink && (
-    <GoToEditAction
-      contactId={contact.id}
-      rootClass={clsx(classes.action, classes.editAction)}
-      iconClass={classes.icon}
-    />
+    <GoToEditAction contactId={contact.id} iconClass={classes.icon} />
   )
 
   const deleteAction = showDeleteButton && (
-    <DeleteAction
-      onConfirm={onDelete}
-      rootClass={clsx(classes.action, classes.deleteAction)}
-      iconClass={classes.icon}
-    />
+    <DeleteAction onConfirm={onDelete} iconClass={classes.icon} />
   )
 
-  return isLinkToDetails ? (
-    <div className="relative h-full w-full">
-      <Link href={contactDetailsUrl(contact.id)} className={clsx("block h-full", classes.rootLink)}>
-        {avatarAndName}
-      </Link>
-      {favoriteAction}
-      {editAction}
-      {deleteAction}
-    </div>
-  ) : (
-    <div className="relative h-full w-full" onClick={onSelect}>
-      {avatarAndName}
-      {favoriteAction}
-      {editAction}
-      {deleteAction}
+  return (
+    <div
+      className="relative flex h-full w-full items-center justify-between border-solid border-secondary-light shadow-none"
+      onClick={isLinkToDetails ? undefined : onSelect}
+    >
+      {avatar}
+      {name}
+      <span>
+        {favoriteAction}
+        {editAction}
+        {deleteAction}
+      </span>
     </div>
   )
 }
