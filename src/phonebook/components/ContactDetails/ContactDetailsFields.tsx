59614d6f9a01af1b0f8232c8c92ec055e5dff4a4
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-05 18:44:39 +0100

    tailwind +

--- src/phonebook/components/ContactDetails/ContactDetailsFields.tsx
+++ src/phonebook/components/ContactDetails/ContactDetailsFields.tsx
@@ -1,32 +1,28 @@
 import { Contact } from "../../models"
-import style from "./ContactDetailsFields.style"
-import { List, ListItem, withStyles, WithStyles, Typography } from "@material-ui/core"
 import { ContactFieldLabel } from "../various"
 import EmailIcon from "@material-ui/icons/Email"
 import PhoneIcon from "@material-ui/icons/Phone"
 
-export default withStyles(style)(
-  ({ contact, classes }: { contact: Contact } & WithStyles<typeof style>) => (
-    <div className={classes.root}>
-      <div className={classes.field}>
-        <div className={classes.labelContainer}>
-          <ContactFieldLabel Icon={EmailIcon} text="email" />
-        </div>
-        <Typography className={classes.fieldValue}>{contact.email}</Typography>
+export default ({ contact }: { contact: Contact }) => (
+  <div>
+    <div className="py-5">
+      <div className="md:flex md:h-full md:w-32 md:flex-col">
+        <ContactFieldLabel Icon={EmailIcon} text="email" />
       </div>
-      <div className={classes.field}>
-        <div className={classes.labelContainer}>
-          <ContactFieldLabel Icon={PhoneIcon} text="number" />
-        </div>
-        <List disablePadding className={classes.fieldValue}>
-          {contact.numbers.map(({ label, value }) => (
-            <ListItem disableGutters key={value}>
-              <Typography className="w-24 uppercase">{label}</Typography>
-              <Typography className="underline">{value}</Typography>
-            </ListItem>
-          ))}
-        </List>
-      </div>
+      <p className="ml-10 mt-3">{contact.email}</p>
     </div>
-  ),
+    <div className="py-5">
+      <div className="md:flex md:h-full md:w-32 md:flex-col">
+        <ContactFieldLabel Icon={PhoneIcon} text="number" />
+      </div>
+      <ul className="ml-10 mt-4 flex flex-col gap-4">
+        {contact.numbers.map(({ label, value }) => (
+          <li key={value} className="flex gap-2">
+            <p className="w-24 uppercase">{label}</p>
+            <p className="underline">{value}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
 )
