c762776d9b4a1cda17b93602ba7b91837b023293
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-08 17:36:51 +0100

    main +

--- src/phonebook/components/ContactDetails/ContactDetailsFields.tsx
+++ src/phonebook/components/ContactDetails/ContactDetailsFields.tsx
@@ -6,15 +6,11 @@ import PhoneIcon from "@material-ui/icons/Phone"
 export default ({ contact }: { contact: Contact }) => (
   <div>
     <div className="py-5">
-      <div className="md:flex md:h-full md:w-32 md:flex-col">
-        <ContactFieldLabel Icon={EmailIcon} text="email" />
-      </div>
+      <ContactFieldLabel Icon={EmailIcon} text="email" />
       <p className="ml-10 mt-3">{contact.email}</p>
     </div>
     <div className="py-5">
-      <div className="md:flex md:h-full md:w-32 md:flex-col">
-        <ContactFieldLabel Icon={PhoneIcon} text="number" />
-      </div>
+      <ContactFieldLabel Icon={PhoneIcon} text="number" />
       <ul className="ml-10 mt-4 flex flex-col gap-4">
         {contact.numbers.map(({ label, value }) => (
           <li key={value} className="flex gap-2">
