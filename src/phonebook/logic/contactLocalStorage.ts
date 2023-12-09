f4208605de4474a51a44953cfbabd26c45dd70c2
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-09 10:32:00 +0100

    main +

--- src/phonebook/logic/contactLocalStorage.ts
+++
@@ -1,9 +0,0 @@
-import { Contact } from "../models"
-
-export const getContacts = () => {
-  const contactListJsonOrNothing = localStorage.getItem("contacts")
-  return !contactListJsonOrNothing ? undefined : (JSON.parse(contactListJsonOrNothing) as Contact[])
-}
-
-export const persistContacts = (contactList: Contact[]) =>
-  localStorage.setItem("contacts", JSON.stringify(contactList))
