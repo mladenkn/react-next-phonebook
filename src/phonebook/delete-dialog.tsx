8e58c187dc851590c24d8fb6b0332621ba7099a6
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-09 22:10:11 +0100

    delete-dialog +

--- src/phonebook/delete-dialog.tsx
+++ src/phonebook/delete-dialog.tsx
@@ -1,47 +1,31 @@
-import {
-  WithStyles,
-  withStyles,
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-} from "@material-ui/core"
-import style from "./delete-dialog-style"
+import { Dialog } from "@material-ui/core"
 
 type Props = {
   text: string
   onCancel: () => void
   onConfirm: () => void
   isOpen: boolean
-} & WithStyles<typeof style>
+}
 
-const DeleteModal = ({ classes, text, isOpen, onCancel, onConfirm }: Props) => (
-  <Dialog className={classes.root} open={isOpen} aria-labelledby="delete-dialog">
-    <DialogTitle className={classes.title} id="delete-dialog">
-      Delete
-    </DialogTitle>
+const DeleteModal = ({ text, isOpen, onCancel, onConfirm }: Props) => (
+  <Dialog className="rounded-sm" open={isOpen}>
+    <h1 className="mx-6 mb-4 mt-6 text-xl">Delete</h1>
 
-    <DialogContent>
-      <DialogContentText className={classes.text}>{text}</DialogContentText>
-    </DialogContent>
+    <p className="mx-6 mb-6 mt-4 text-primary-dark">{text}</p>
 
-    <DialogActions className={classes.actions}>
-      <Button variant="contained" color="secondary" onClick={onCancel} className={classes.button}>
+    <div className="mx-6 mb-4 mt-2 flex justify-between gap-10">
+      <button className="w-36 rounded-lg bg-secondary-main py-2 text-white" onClick={onCancel}>
         Cancel
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
+      </button>
+      <button
+        className="w-36 rounded-lg bg-primary-main py-2 text-white"
         onClick={onConfirm}
-        className={classes.button}
         autoFocus
       >
         Delete
-      </Button>
-    </DialogActions>
+      </button>
+    </div>
   </Dialog>
 )
 
-export default withStyles(style)(DeleteModal)
+export default DeleteModal
