5fa0d7c592ff487049c3baa39074fe1595dc544a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-10 22:15:17 +0100

    +

--- src/phonebook/text-input-dialog.tsx
+++ src/phonebook/text-input-dialog.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { Dialog, DialogContent, DialogTitle, Input, DialogActions, Button } from "@material-ui/core"
-import { useTextInputDialogStyle } from "./various-style"
 
 type TextInputDialogProps = {
   text?: string
@@ -10,20 +9,19 @@ type TextInputDialogProps = {
 }
 
 export const TextInputDialog = (p: TextInputDialogProps) => {
-  const classes = useTextInputDialogStyle()
   const [input, setInput] = useState(p.text || "")
   const inputValid = p.isInputValid(input)
   const okButtonDisabled = !inputValid
 
   return (
-    <Dialog classes={{ paper: classes.paper }} open={true} aria-labelledby="text-input-dialog">
+    <Dialog classes={{ paper: "mx-2 w-full" }} open={true} aria-labelledby="text-input-dialog">
       <DialogTitle id="text-input-dialog">Enter image url:</DialogTitle>
 
       <DialogContent>
-        <Input className={classes.input} value={input} onChange={e => setInput(e.target.value)} />
+        <Input className="w-full sm:w-96" value={input} onChange={e => setInput(e.target.value)} />
       </DialogContent>
 
-      <DialogActions className={classes.actions}>
+      <DialogActions className="mx-6 mb-3 mt-2">
         <Button variant="contained" color="secondary" onClick={() => p.onCancel()} className={""}>
           Cancel
         </Button>
