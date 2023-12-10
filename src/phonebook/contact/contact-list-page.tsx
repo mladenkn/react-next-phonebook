fa6c9c09e135d92df487ee56bb63abc24afc1f0a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-10 12:10:39 +0100

    pages +

--- src/phonebook/contact/contact-list-page.tsx
+++ src/phonebook/contact/contact-list-page.tsx
@@ -3,7 +3,7 @@ import ContactList from "../contact-list"
 import { useContactListOps } from "./contact-list-ops"
 import SearchIcon from "@material-ui/icons/Search"
 import clsx from "clsx"
-import { tw } from "../../utils"
+import { cn, tw } from "../../utils"
 
 const StyledTab = tw.button`text-lg text-tc-primary font-semibold`
 
@@ -18,38 +18,36 @@ const ContactListPage = ({ className }: { className?: string }) => {
   const ops = useContactListOps()
 
   return (
-    <div className={className}>
-      <div className="flex max-w-5xl flex-col items-center">
-        <div className="flex gap-8">
-          <StyledTab
-            className={clsx(currentTab === 0 && "text-tc-secondary")}
-            onClick={() => setCurrentTab(0)}
-          >
-            All contacts
-          </StyledTab>
-          <div className="h-5 w-1 bg-secondary-main" />
-          <StyledTab
-            className={clsx(currentTab === 1 && "text-tc-secondary")}
-            onClick={() => setCurrentTab(1)}
-          >
-            My favorites
-          </StyledTab>
-        </div>
-        <div className="mt-2 h-0.25 w-full bg-primary-main sm:mt-6" />
-        <div className={searchWrapper_class}>
-          <SearchIcon className="ml-2 mr-2 text-gray-500" />
-          <input className="h-12 w-full p-2 text-lg text-gray-500 outline-none" />
-        </div>
-        {ops.fetchStatus === "COMPLETED" && (
-          <ContactList
-            contacts={currentTab === 0 ? ops.contacts!.all : ops.contacts!.favorites}
-            deleteContact={ops.deleteContact}
-            toggleFavorite={ops.toggleFavorite}
-            includeAdder
-            className="mt-3 sm:mt-6"
-          />
-        )}
+    <div className={cn("flex max-w-5xl flex-col items-center", className)}>
+      <div className="flex gap-8">
+        <StyledTab
+          className={clsx(currentTab === 0 && "text-tc-secondary")}
+          onClick={() => setCurrentTab(0)}
+        >
+          All contacts
+        </StyledTab>
+        <div className="h-5 w-1 bg-secondary-main" />
+        <StyledTab
+          className={clsx(currentTab === 1 && "text-tc-secondary")}
+          onClick={() => setCurrentTab(1)}
+        >
+          My favorites
+        </StyledTab>
       </div>
+      <div className="mt-2 h-0.25 w-full bg-primary-main sm:mt-6" />
+      <div className={searchWrapper_class}>
+        <SearchIcon className="ml-2 mr-2 text-gray-500" />
+        <input className="h-12 w-full p-2 text-lg text-gray-500 outline-none" />
+      </div>
+      {ops.fetchStatus === "COMPLETED" && (
+        <ContactList
+          contacts={currentTab === 0 ? ops.contacts!.all : ops.contacts!.favorites}
+          deleteContact={ops.deleteContact}
+          toggleFavorite={ops.toggleFavorite}
+          includeAdder
+          className="mt-3 sm:mt-6"
+        />
+      )}
     </div>
   )
 }
