125b933b08883669cb5607438c30af36c3f320e7
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-14 16:32:12 +0100

    tailwind +

--- src/phonebook/contact/contact-list-adder-style.ts
+++ src/phonebook/contact/contact-list-adder-style.ts
@@ -2,19 +2,6 @@ import { createStyles, Theme } from "@material-ui/core"
 
 export default ({ palette, breakpoints }: Theme) =>
   createStyles({
-    root: {
-      padding: 0,
-      width: "100%",
-      height: "100%",
-      borderColor: palette.primary.light,
-      borderStyle: "dashed",
-      borderWidth: 1,
-      boxShadow: "inherit",
-      display: "flex",
-      "&:hover": {
-        textDecoration: "none",
-      },
-    },
     icon: {
       color: palette.primary.light,
       fontSize: 27,
@@ -34,12 +21,6 @@ export default ({ palette, breakpoints }: Theme) =>
         justifyContent: "space-around",
         marginLeft: 20,
         width: 110,
-      },
-    },
-    [breakpoints.up("md")]: {
-      root: {
-        justifyContent: "center",
-        alignItems: "center",
       },
     },
   })
--- src/phonebook/contact/contact-list-adder.tsx
+++ src/phonebook/contact/contact-list-adder.tsx
@@ -5,7 +5,10 @@ import { Add } from "@material-ui/icons"
 import { Link } from "react-router-dom"
 
 const ContactAdder = ({ classes }: WithStyles<typeof style>) => (
-  <Link to={contactCreateUrl} className={classes.root}>
+  <Link
+    to={contactCreateUrl}
+    className="flex h-full w-full border-1 border-dashed border-primary-light md:items-center md:justify-center"
+  >
     <div className={classes.content_}>
       <Add className={classes.icon} />
       <p className="text-primary-light">Add new</p>
