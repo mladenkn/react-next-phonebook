70a3c1c8f4a17ae8306ccac96ac6c216cc0afb4b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-15 17:28:29 +0100

    no-material-dialog +

--- src/phonebook/delete-dialog.tsx
+++ src/phonebook/delete-dialog.tsx
@@ -8,24 +8,44 @@ type Props = {
 }
 
 const DeleteModal = ({ text, isOpen, onCancel, onConfirm }: Props) => (
-  <Dialog className="rounded-sm" open={isOpen}>
-    <h1 className="mx-6 mb-4 mt-6 text-xl">Delete</h1>
+  // <div className="fixed inset-0 flex items-center justify-center overflow-y-auto bg-white">
+  //   <h1 className="mx-6 mb-4 mt-6 text-xl">Delete</h1>
 
-    <p className="mx-6 mb-6 mt-4 text-primary-dark">{text}</p>
+  //   <p className="mx-6 mb-6 mt-4 text-primary-dark">{text}</p>
 
-    <div className="mx-6 mb-4 mt-2 flex justify-between gap-10">
-      <button className="w-36 rounded-lg bg-secondary-main py-2 text-white" onClick={onCancel}>
-        Cancel
-      </button>
-      <button
-        className="w-36 rounded-lg bg-primary-main py-2 text-white"
-        onClick={onConfirm}
-        autoFocus
-      >
-        Delete
-      </button>
+  //   <div className="mx-6 mb-4 mt-2 flex justify-between gap-10">
+  //     <button className="w-36 rounded-lg bg-secondary-main py-2 text-white" onClick={onCancel}>
+  //       Cancel
+  //     </button>
+  //     <button
+  //       className="w-36 rounded-lg bg-primary-main py-2 text-white"
+  //       onClick={onConfirm}
+  //       autoFocus
+  //     >
+  //       Delete
+  //     </button>
+  //   </div>
+  // </div>
+  <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
+    <div className="rounded bg-white p-8 shadow-lg">
+      <p className="text-lg">
+        Hello, I'm a basic modal! sdf asdčlfk jasdčfklj asdč fjasčdfj čdklsjf ačkdfj as df
+        asdfčahjsdf kasdf ajsdf a sdf asdćfkjasdč fj
+      </p>
+      <p className="text-lg">
+        Hello, I'm a basic modal! sdf asdčlfk jasdčfklj asdč fjasčdfj čdklsjf ačkdfj as df
+        asdfčahjsdf kasdf ajsdf a sdf asdćfkjasdč fj
+      </p>
+      <p className="text-lg">
+        Hello, I'm a basic modal! sdf asdčlfk jasdčfklj asdč fjasčdfj čdklsjf ačkdfj as df
+        asdfčahjsdf kasdf ajsdf a sdf asdćfkjasdč fj
+      </p>
+      <p className="text-lg">
+        Hello, I'm a basic modal! sdf asdčlfk jasdčfklj asdč fjasčdfj čdklsjf ačkdfj as df
+        asdfčahjsdf kasdf ajsdf a sdf asdćfkjasdč fj
+      </p>
     </div>
-  </Dialog>
+  </div>
 )
 
 export default DeleteModal
