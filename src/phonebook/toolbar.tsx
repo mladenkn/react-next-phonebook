e96b23ba4aa90778f17a9accac1b555d6964728b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-15 19:52:44 +0100

    no-material +

--- src/phonebook/toolbar.tsx
+++ src/phonebook/toolbar.tsx
@@ -1,18 +1,18 @@
 import { homePageUrl } from "./urls"
-import { Toolbar as MuiToolbar, AppBar } from "@material-ui/core"
+import { AppBar } from "@material-ui/core"
 import { Link } from "react-router-dom"
 
 export default function Toolbar() {
   return (
     <AppBar style={{ zIndex: 20 }}>
-      <MuiToolbar className="flex h-10 justify-center bg-gradient-to-r from-primary-dark to-primary-light">
+      <div className="flex h-11 items-center justify-center bg-gradient-to-r from-primary-dark to-primary-light">
         <Link
-          className="pb-2 pt-3 text-xl font-semibold uppercase text-white no-underline"
+          className="mt-1 text-xl font-semibold uppercase text-white no-underline"
           to={homePageUrl}
         >
           Phonebook
         </Link>
-      </MuiToolbar>
+      </div>
       <div
         style={{ backgroundImage: "linear-gradient(to right, #70BBC3, #A0D8DC)" }}
         className="h-2"
