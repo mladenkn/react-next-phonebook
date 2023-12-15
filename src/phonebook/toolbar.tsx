4ba74c5c692c3d60ec0f545fd484ca6365eeae4d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-15 17:11:51 +0100

    toolbar +

--- src/phonebook/toolbar.tsx
+++ src/phonebook/toolbar.tsx
@@ -1,13 +1,11 @@
 import { homePageUrl } from "./urls"
-import { Typography, Toolbar as MuiToolbar, AppBar } from "@material-ui/core"
-import { createStyles, makeStyles, Theme } from "@material-ui/core"
+import { Toolbar as MuiToolbar, AppBar } from "@material-ui/core"
 import { Link } from "react-router-dom"
 
 export const Toolbar = () => {
-  const classes = useToolbarStyle()
   return (
     <AppBar>
-      <MuiToolbar className={classes.toolbar}>
+      <MuiToolbar className="relative flex h-10 justify-center bg-gradient-to-r from-primary-dark to-primary-light">
         <Link
           className="pb-2 pt-3 text-xl font-semibold uppercase text-white no-underline"
           to={homePageUrl}
@@ -15,26 +13,10 @@ export const Toolbar = () => {
           Phonebook
         </Link>
       </MuiToolbar>
-      <div className={classes.toolbarBorder} />
+      <div
+        style={{ backgroundImage: "linear-gradient(to right, #70BBC3, #A0D8DC)" }}
+        className="h-2"
+      />
     </AppBar>
   )
 }
-
-export const toolbarStyle = ({ palette }: Theme) =>
-  createStyles({
-    toolbar: {
-      backgroundImage: `linear-gradient(to right, ${palette.primary.dark}, ${palette.primary.light})`,
-      minHeight: 40,
-      display: "flex",
-      justifyContent: "center",
-      position: "relative",
-    },
-    toolbarBorder: {
-      backgroundImage: `linear-gradient(to right, #70BBC3, #A0D8DC)`,
-      height: 7,
-    },
-  })
-
-export const useToolbarStyle = makeStyles(toolbarStyle, {
-  name: "ToolbarStyle",
-})
