cd7769b102489351604440a17f39810327df6ce2
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-15 18:24:20 +0100

    no-material-dialog +

--- src/phonebook/delete-dialog.tsx
+++ src/phonebook/delete-dialog.tsx
@@ -8,24 +8,20 @@ type Props = {
 
 const DeleteModal = ({ text, onCancel, onConfirm }: Props) => (
   <Modal contentClassName="rounded bg-white p-4 shadow-lg">
-    <>
-      <h1 className="mx-6 mb-4 mt-6 text-xl">Delete</h1>
-
-      <p className="mx-6 mb-6 mt-4 text-primary-dark">{text}</p>
-
-      <div className="mx-6 mb-4 mt-2 flex justify-between gap-10">
-        <button className="w-36 rounded-lg bg-secondary-main py-2 text-white" onClick={onCancel}>
-          Cancel
-        </button>
-        <button
-          className="w-36 rounded-lg bg-primary-main py-2 text-white"
-          onClick={onConfirm}
-          autoFocus
-        >
-          Delete
-        </button>
-      </div>
-    </>
+    <h1 className="mx-6 mb-4 mt-6 text-xl">Delete</h1>
+    <p className="mx-6 mb-6 mt-4 text-primary-dark">{text}</p>
+    <div className="mx-6 mb-4 mt-2 flex justify-between gap-10">
+      <button className="w-36 rounded-lg bg-secondary-main py-2 text-white" onClick={onCancel}>
+        Cancel
+      </button>
+      <button
+        className="w-36 rounded-lg bg-primary-main py-2 text-white"
+        onClick={onConfirm}
+        autoFocus
+      >
+        Delete
+      </button>
+    </div>
   </Modal>
 )
 
