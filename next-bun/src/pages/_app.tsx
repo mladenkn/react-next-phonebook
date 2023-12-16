7c3327a5901dd6801e5abccd7013efb34135cc03
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-16 19:48:46 +0100

    next setup +

--- next-bun/src/pages/_app.tsx
+++ next-bun/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
+import "~/styles.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
