81d9691a95ef5e7ac6fc7510fdcdf5ade631020b
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-16 20:24:55 +0100

    next app +

--- next-bun/tailwind.config.ts
+++ next-bun/tailwind.config.ts
@@ -1,10 +1,39 @@
-import { type Config } from "tailwindcss";
+const { colors } = require("@material-ui/core")
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: "#78C9CE",
+          main: "#2DA1AD",
+          dark: "#2496A2",
+        },
+        secondary: {
+          light: "#E3E3E3",
+          main: "#BBC4C3",
+          dark: colors.grey[600]
+        },
+        tc: {
+          primary: colors.grey[500],
+          secondary: "#2DA1AD",
+        },
+      },
+      boxShadow: {
+        homeSearch: "0 0 12px",
+      },
+      height: {
+        0.25: "0.062rem"
+      },
+      borderWidth: {
+        1: "1px"
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
@@ -12,3 +41,4 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
