fad229f20bc00a0010d3da739751927ed5a513c2
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-16 18:39:14 +0100

    starting out with next +

--- next/drizzle.config.ts
+++ next/drizzle.config.ts
@@ -1,10 +1,12 @@
 import { type Config } from "drizzle-kit";
 
+import { env } from "~/env";
+
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: "TODO",
+    connectionString: env.DATABASE_URL,
   },
   tablesFilter: ["phonebook_*"],
 } satisfies Config;
