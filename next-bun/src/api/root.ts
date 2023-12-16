f1570771497dbe080c9b0a3a0a0ee8ab3500cb86
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-16 19:00:30 +0100

    next setup +

---
+++ next-bun/src/api/root.ts
@@ -0,0 +1,22 @@
+import { createTRPCRouter } from "~/api/trpc";
+import { publicProcedure } from "~/api/trpc"
+import { z } from "zod";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  example: createTRPCRouter({
+    hello: publicProcedure.input(z.object({ text: z.string() }))
+      .query(({ input }) => {
+        return {
+          greeting: `Hello ${input.text}`,
+        };
+      }),
+  })
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
