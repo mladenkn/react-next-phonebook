02a5d44ec91d264b3f2ad67542cf4a5c4d2b7f7d
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-16 22:29:46 +0100

    next app +

--- next-bun/src/contact/contact-list-page.tsx
+++ next-bun/src/contact/contact-list-page.tsx
@@ -44,7 +44,7 @@ const ContactListPage = ({ data }: Props) => {
         </div>
         <div className="mt-2 h-0.25 w-full bg-primary-main sm:mt-6" />
         <div className={searchWrapper_class}>
-          <SearchIcon color="secondary" className="ml-2 mr-2 h-4 w-4" />
+          <SearchIcon color="secondary" className="ml-2 mr-2" />
           <input className="h-12 w-full p-2 text-lg text-gray-500 outline-none" />
         </div>
         <ContactList
