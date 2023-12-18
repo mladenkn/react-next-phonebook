02033e10c3a50ba9e6c0e9bcc1ae7b20a076f80a
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-19 00:37:52 +0100

    next | format +

--- next-bun/src/pages/contact/[id].tsx
+++ next-bun/src/pages/contact/[id].tsx
@@ -1,17 +1,18 @@
-import { useRouter } from 'next/router'
-import contactsData from '~/contact/contact-data'
-import ContactDetailsPage from '~/contact/contact-details-page'
-import Toolbar from '~/toolbar'
-import { asNonNil } from '~/utils'
-import { getBreakpointContainerStyle } from '~/utils/ui-utils'
+import { useRouter } from "next/router"
+import contactsData from "~/contact/contact-data"
+import ContactDetailsPage from "~/contact/contact-details-page"
+import Toolbar from "~/toolbar"
+import { asNonNil } from "~/utils"
+import { getBreakpointContainerStyle } from "~/utils/ui-utils"
 
-
-export default function ContactDetailsPageWrapper(){
+export default function ContactDetailsPageWrapper() {
   const router = useRouter()
   const contactId = router.query.id as any
   const contact = asNonNil(contactsData.find(c => c.id == contactId))
-  return <div className={getBreakpointContainerStyle("md")}>
-    <Toolbar />
-    <ContactDetailsPage className='mt-14 md:mt-24' contact={contact} onFavorite={() => {}} />
-  </div>
+  return (
+    <div className={getBreakpointContainerStyle("md")}>
+      <Toolbar />
+      <ContactDetailsPage className="mt-14 md:mt-24" contact={contact} onFavorite={() => {}} />
+    </div>
+  )
 }
