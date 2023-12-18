999f6e1bb20955e4a869b7d25978ae51078e8d22
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-18 18:13:18 +0100

    next-icons-2 +

--- next-bun/src/assets/icons.tsx
+++ next-bun/src/assets/icons.tsx
@@ -5,61 +5,37 @@ type Props = {
 }
 
 export const MagnifierIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
   </svg>
 )
 
 export const PlusIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
   </svg>
 )
 
 export const HeartFilledIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
   </svg>
 )
 
 export const HeartBorderIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z" />
   </svg>
 )
 
 export const PencilIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
   </svg>
 )
 
 export const TrashIcon = ({className}: Props) => (
-  <svg
-    aria-hidden="true"
-    className={cn(defaultStyle, className)}
-    viewBox="0 0 24 24"
-  >
+  <svg className={cn(defaultStyle, className)} viewBox="0 0 24 24">
     <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
   </svg>
 )
