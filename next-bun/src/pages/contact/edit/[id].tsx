d398d12dd74a033c7c21b02a997bf7b5cc884e6f
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-19 18:18:53 +0100

    contact-details-refac +

---
+++ next-bun/src/pages/contact/edit/[id].tsx
@@ -0,0 +1,20 @@
+import { GetServerSidePropsContext } from "next"
+import contactsData from "~/contact/contact-data"
+import ContactEditPage from "~/contact/contact-edit"
+import { asNonNil } from "~/utils"
+
+export function getServerSideProps({ query }: GetServerSidePropsContext){
+  if(typeof query.id !== "string")
+    throw new Error ()
+  const contactId = parseInt(query.id)
+  const contact = asNonNil(contactsData.find(c => c.id == contactId))
+  return {
+    props: { contact }
+  }
+}
+
+type Props = ReturnType<typeof getServerSideProps>["props"]
+
+export default function ContactDetailsPageWrapper({ contact }: Props) {
+  return <ContactEditPage contact={contact} onSave={() => {}} />
+}
