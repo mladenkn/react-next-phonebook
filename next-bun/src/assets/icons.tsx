24b2d84f5d8cb1298dd32e72788727d8d66b4d68
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-19 19:41:21 +0100

    contact-details-refac +

--- next-bun/src/assets/icons.tsx
+++ next-bun/src/assets/icons.tsx
@@ -3,7 +3,6 @@ import { cn } from "~/utils"
 
 type Props = {
   className?: string
-  style?: CSSProperties
 }
 
 export const MagnifierIcon = ({ className }: Props) => (
@@ -82,7 +81,7 @@ export const AddCircleOutlineIcon = ({ className }: Props) => (
   </svg>
 )
 
-export const RemoveIcon = ({ className, style }: Props) => (
+export const RemoveIcon = ({ className, style }: Props & { style?: CSSProperties }) => (
   <svg className={cn(defaultStyle, className)} style={style} viewBox="0 0 24 24">
     <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
   </svg>
