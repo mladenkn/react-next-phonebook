2c4138b65c9c5218bd94a9bfbcc1e198ca02ca15
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-19 19:52:25 +0100

    .

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   schema: "./src/server/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.DATABASE_URL as any,
   },
   tablesFilter: ["phonebook_*"],
 } satisfies Config
