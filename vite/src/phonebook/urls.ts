8906e81bc38f076e802013d6018ca27c0b2eb555
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-19 19:48:16 +0100

    next ++++

--- vite/src/phonebook/urls.ts
+++
@@ -1,4 +0,0 @@
-export const contactEditUrl = (contactId: number) => "/contact/edit/" + contactId
-export const contactDetailsUrl = (contactId: number) => "/contact/" + contactId
-export const contactCreateUrl = "/contact/create"
-export const homePageUrl = "/"
