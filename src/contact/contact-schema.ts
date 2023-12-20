24490502817ead7fb059355a1c5d5d990f64f521
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-20 15:18:18 +0100

    database | schema +

--- src/contact/contact-schema.ts
+++ src/contact/contact-schema.ts
@@ -2,11 +2,15 @@ import {
   pgTable,
   varchar,
   serial,
-  json
+  json,
+  boolean
 } from "drizzle-orm/pg-core"
 
 export const User = pgTable("Contact", {
   id: serial("id").primaryKey().notNull(),
-  // name: varchar("name", { length: 32 }),
-  // avatarStyle: json("avatarStyle"),    
+  fullName: varchar("name", { length: 64 }).notNull(),
+  avatarStyle: json("avatarStyle"),
+  avatarUrl: varchar("avatarUrl"),
+  email: varchar("email").notNull(),
+  isFavorite: boolean("isFavorite").notNull().default(false)
 })
