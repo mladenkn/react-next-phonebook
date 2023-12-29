94ea397ec0851af08805f2522cfdef81301b0cca
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2023-12-29 20:12:52 +0100

    no-edit .

--- src/actions.tsx
+++ src/actions.tsx
@@ -11,7 +11,7 @@ type GoToEditActionProps = {
 export function GoToEditAction({ contactId }: GoToEditActionProps) {
   function handleClick(e: MouseEvent) {
     e.stopPropagation()
-    alert("Edit functionality not implemented. HARE KRISHNA JAAAY")
+    alert("Edit functionality not implemented.")
   }
   return (
     <button onClick={handleClick}>
