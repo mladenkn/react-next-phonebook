cf33cf90e22197b5115ae50dc04decb005af7621
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-01-02 19:34:14 +0100

    toast +

--- src/actions.tsx
+++ src/actions.tsx
@@ -9,22 +9,6 @@ type GoToEditActionProps = {
   contactId: number
 }
 
-const toastCn = cn([
-  // "data-[state=open]:animate-slideIn",
-  // "data-[state=closed]:animate-hide",
-  // "data-[swipe=end]:animate-swipeOut",
-  // // "grid",
-  // // "grid-cols-[auto_max-content]",
-  // "items-center",
-  // // "gap-x-[15px]",
-  // // "p-[15px]",
-  // "shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]",
-  // "[grid-template-areas:_'title_action'_'description_action']",
-  // "data-[swipe=cancel]:translate-x-0",
-  // "data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)]",
-  // "data-[swipe=cancel]:transition-[transform_200ms_ease-out]",
-])
-
 export function GoToEditAction({ contactId }: GoToEditActionProps) {
   const [toastActive, setToastActive] = useState(false)
   const timerRef = useRef(0)
@@ -37,6 +21,7 @@ export function GoToEditAction({ contactId }: GoToEditActionProps) {
       setToastActive(true)
     }, 100)
   }
+
   return (
     <>
       <button onClick={handleClick}>
