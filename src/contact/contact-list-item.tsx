2cf81779c6ca561651bb441cdb4c4e23b92aa1b1
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-09 13:32:14 +0200

    text not selectable

--- src/contact/contact-list-item.tsx
+++ src/contact/contact-list-item.tsx
@@ -33,7 +33,9 @@ export default function ContactListItem({
     />
   )
 
-  const name = <p className="font-sans text-secondary-dark md:text-center">{contact.fullName}</p>
+  const name = (
+    <p className="select-none font-sans text-secondary-dark md:text-center">{contact.fullName}</p>
+  )
 
   const favoriteAction = <ContactFavorite id={contact.id} isFavorite={contact.isFavorite} />
   const editAction = (
