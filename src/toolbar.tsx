5b07f4fcad8170a7939de96353ac0946befdd0f5
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-09 10:50:51 +0200

    added source code icon 3

--- src/toolbar.tsx
+++ src/toolbar.tsx
@@ -22,7 +22,9 @@ export default function Toolbar({ titleContainerClassName }: Props) {
           >
             Phonebook
           </Link>
-          <SourceCodeIcon />
+          <a href="https://github.com/mladenkn/react-typescript-phonebook" target="_blank">
+            <SourceCodeIcon />
+          </a>
         </div>
       </div>
       <div
