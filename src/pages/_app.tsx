1905d4f08ae51086ae1dbae722fb9dcaf47d4017
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-12 21:19:39 +0200

    contact-edit 24

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,16 @@ import Head from "next/head"
 import { type AppType } from "next/app"
 import { api } from "~/utils/api"
 import "~/styles.css"
-import * as Toast from "@radix-ui/react-toast"
-import { ToastViewport } from "~/utils/toast"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Toast.Provider swipeDirection="right">
+    <>
       <Head>
         <title>Phonebook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-      <ToastViewport />
-    </Toast.Provider>
+    </>
   )
 }
 
