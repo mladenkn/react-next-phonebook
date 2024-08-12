38f3b21cb9593d892ae2f2af97f4f87ec497f726
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-12 19:18:44 +0200

    contact-edit 10

--- src/contact/contact-edit.tsx
+++ src/contact/contact-edit.tsx
@@ -1,12 +1,6 @@
 import { Contact as _Contact } from "../models"
-import { GoBackAction } from "../actions"
 import ContactForm from "./contact-form"
-import SwapableAvatar from "../swapable-avatar"
-import { cn } from "~/utils/ui-utils"
-import { useRouter } from "next/router"
 import FixedToolbar from "~/toolbar"
-import { ContactDeleteAction } from "./contact-delete"
-import { homePageUrl } from "~/urls"
 
 type Contact = Omit<_Contact, "id"> & { id?: number }
 
@@ -15,37 +9,10 @@ type Props = {
 }
 
 export default function ContactEdit({ contact }: Props) {
-  const avatar = (
-    <SwapableAvatar src={contact.avatarUrl} className="h-52 w-52" onChange={() => {}} />
-  )
-
-  const router = useRouter()
-
   return (
     <div className="mx-auto max-w-3xl">
       <FixedToolbar />
-      <div className={cn("mt-16 px-3 text-secondary-dark md:mt-24 md:flex sm-max:w-full")}>
-        <div className="mr-4 sm-max:hidden">{avatar}</div>
-        <div>
-          <div className="mt-4 flex w-full items-center justify-between px-1 pb-2 pt-0">
-            <GoBackAction />
-            {contact.id ? (
-              <ContactDeleteAction
-                contactId={contact.id}
-                onComplete={() => router.push(homePageUrl)}
-                withHoverEffect
-                withText
-              />
-            ) : null}
-          </div>
-          <div className="h-0.25 w-full bg-secondary-main md:hidden" />
-          <h1 className="flex items-center justify-center px-0 md:hidden md:pb-2 sm-max:py-4">
-            {avatar}
-          </h1>
-          <div className="mb-2 h-0.25 w-full bg-primary-main" />
-          <ContactForm initialInput={contact} />
-        </div>
-      </div>
+      <ContactForm initialInput={contact} />
     </div>
   )
 }
--- src/contact/contact-form.tsx
+++ src/contact/contact-form.tsx
@@ -10,6 +10,8 @@ import {
 } from "~/assets/icons"
 import { useForm } from "@tanstack/react-form"
 import { useRouter } from "next/router"
+import { GoBackAction } from "../actions"
+import SwapableAvatar from "../swapable-avatar"
 
 const styles = {
   input: "p-2 border-2 border-solid border-secondary-light text-secondary-main outline-none",
@@ -35,116 +37,141 @@ export default function ContactForm({ initialInput }: Props) {
 
   const router = useRouter()
 
+  const avatar = (
+    <SwapableAvatar src={initialInput.avatarUrl} className="h-52 w-52" onChange={() => {}} />
+  )
+
   return (
     <form
+      className="mt-16 px-3 text-secondary-dark md:mt-20 md:flex sm-max:w-full"
       onSubmit={e => {
         e.preventDefault()
         e.stopPropagation()
         form.handleSubmit()
       }}
     >
-      <label>
-        <ContactFieldLabel
-          icon={<PersonOutlinedIcon className="text-primary-main" />}
-          text="full name"
-          className="mb-2"
-        />
-
-        <form.Field
-          name="fullName"
-          children={field => (
-            <input
-              className={cn(styles.input, "w-full sm:w-1/2")}
-              type="text"
-              name={field.name}
-              onBlur={field.handleBlur}
-              value={field.state.value}
-              onChange={e => field.handleChange(e.target.value)}
+      <div className="mr-4 sm-max:hidden">{avatar}</div>
+      <div>
+        <div className="mt-4 flex w-full items-center justify-between px-1 pb-2 pt-0">
+          <GoBackAction />
+          {/* {initialInput.id ? (
+            <ContactDeleteAction
+              contactId={initialInput.id}
+              onComplete={() => router.push(homePageUrl)}
+              withHoverEffect
+              withText
             />
+          ) : null} */}
+        </div>
+        <div className="h-0.25 w-full bg-secondary-main md:hidden" />
+        <h1 className="flex items-center justify-center px-0 md:hidden md:pb-2 sm-max:py-4">
+          {avatar}
+        </h1>
+        <div className="mb-2 h-0.25 w-full bg-primary-main" />
+
+        <label>
+          <ContactFieldLabel
+            icon={<PersonOutlinedIcon className="text-primary-main" />}
+            text="full name"
+            className="mb-2"
+          />
+
+          <form.Field
+            name="fullName"
+            children={field => (
+              <input
+                className={cn(styles.input, "w-full sm:w-1/2")}
+                type="text"
+                name={field.name}
+                onBlur={field.handleBlur}
+                value={field.state.value}
+                onChange={e => field.handleChange(e.target.value)}
+              />
+            )}
+          />
+        </label>
+
+        <div className="my-4 h-0.25 w-full bg-primary-main" />
+
+        <label>
+          <ContactFieldLabel
+            icon={<EmailIcon className="text-primary-main" />}
+            text="email"
+            className="mb-2"
+          />
+          <form.Field
+            name="email"
+            children={field => (
+              <input
+                className={cn(styles.input, "w-full sm:w-1/2")}
+                type="text"
+                name={field.name}
+                onBlur={field.handleBlur}
+                value={field.state.value}
+                onChange={e => field.handleChange(e.target.value)}
+              />
+            )}
+          />
+        </label>
+
+        <div className="my-4 h-0.25 w-full bg-primary-main" />
+
+        <form.Field name="phoneNumbers" mode="array">
+          {phoneNumberField => (
+            <div>
+              <ContactFieldLabel
+                className="mb-2"
+                icon={<PhoneIcon className="text-primary-main" />}
+                text="Phone numbers"
+              />
+              {phoneNumberField.state.value?.map((_, i) => (
+                <div className="py-2 md:flex md:justify-between md:gap-2" key={i}>
+                  <form.Field name={`phoneNumbers[${i}].value`}>
+                    {field => (
+                      <input
+                        className={cn(styles.input, "max-sm:mb-2 max-sm:w-full")}
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={e => field.handleChange(e.target.value)}
+                      />
+                    )}
+                  </form.Field>
+                  <form.Field name={`phoneNumbers[${i}].label`}>
+                    {field => (
+                      <input
+                        className={styles.input}
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={e => field.handleChange(e.target.value)}
+                      />
+                    )}
+                  </form.Field>
+                  <button className="max-sm:ml-2" type="button">
+                    <RemoveCircledIcon className="text-secondary-main" />
+                  </button>
+                </div>
+              ))}
+            </div>
           )}
-        />
-      </label>
+        </form.Field>
 
-      <div className="my-4 h-0.25 w-full bg-primary-main" />
-
-      <label>
-        <ContactFieldLabel
-          icon={<EmailIcon className="text-primary-main" />}
-          text="email"
-          className="mb-2"
-        />
-        <form.Field
-          name="email"
-          children={field => (
-            <input
-              className={cn(styles.input, "w-full sm:w-1/2")}
-              type="text"
-              name={field.name}
-              onBlur={field.handleBlur}
-              value={field.state.value}
-              onChange={e => field.handleChange(e.target.value)}
-            />
-          )}
-        />
-      </label>
-
-      <div className="my-4 h-0.25 w-full bg-primary-main" />
-
-      <form.Field name="phoneNumbers" mode="array">
-        {phoneNumberField => (
-          <div>
-            <ContactFieldLabel
-              className="mb-2"
-              icon={<PhoneIcon className="text-primary-main" />}
-              text="Phone numbers"
-            />
-            {phoneNumberField.state.value?.map((_, i) => (
-              <div className="py-2 md:flex md:justify-between md:gap-2" key={i}>
-                <form.Field name={`phoneNumbers[${i}].value`}>
-                  {field => (
-                    <input
-                      className={cn(styles.input, "max-sm:mb-2 max-sm:w-full")}
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={e => field.handleChange(e.target.value)}
-                    />
-                  )}
-                </form.Field>
-                <form.Field name={`phoneNumbers[${i}].label`}>
-                  {field => (
-                    <input
-                      className={styles.input}
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      onBlur={field.handleBlur}
-                      onChange={e => field.handleChange(e.target.value)}
-                    />
-                  )}
-                </form.Field>
-                <button className="max-sm:ml-2" type="button">
-                  <RemoveCircledIcon className="text-secondary-main" />
-                </button>
-              </div>
-            ))}
-          </div>
-        )}
-      </form.Field>
-
-      <button className="mt-6 flex text-primary-main">
-        <AddCircleOutlineIcon className="mr-1.5 text-primary-main" />
-        Add number
-      </button>
-
-      <div className="my-4 h-0.25 w-full bg-primary-main" />
-
-      <div className="mx-0.5 mb-4 mt-6 flex justify-between">
-        <button className={cn(buttonClass, "bg-secondary-main")} onClick={() => router.back()}>
-          Cancel
+        <button className="mt-6 flex text-primary-main">
+          <AddCircleOutlineIcon className="mr-1.5 text-primary-main" />
+          Add number
         </button>
-        <button className={cn(buttonClass, "bg-primary-main")}>Save</button>
+
+        <div className="my-4 h-0.25 w-full bg-primary-main" />
+
+        <div className="mx-0.5 mb-4 mt-6 flex justify-between">
+          <button className={cn(buttonClass, "bg-secondary-main")} onClick={() => router.back()}>
+            Cancel
+          </button>
+          <button className={cn(buttonClass, "bg-primary-main")}>Save</button>
+        </div>
       </div>
     </form>
   )
