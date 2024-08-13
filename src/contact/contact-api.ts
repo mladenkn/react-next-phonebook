ca5727de85c15c49cad7a1ea9b1a0bde69912287
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-13 18:00:20 +0200

    contact-edit 34

--- src/contact/contact-api.ts
+++ src/contact/contact-api.ts
@@ -4,7 +4,7 @@ import { z } from "zod"
 import { eq, desc, and, ilike, or, SQL } from "drizzle-orm"
 import { ContactUpdateInput, ContactCreateInput } from "./contact-api-inputs"
 import { getRandomAvatarStyle } from "./contact-data-generators"
-import { asNonNil, eva, arrayWithoutNils } from "~/utils"
+import { asNonNil, eva } from "~/utils"
 
 const contactApi = createTRPCRouter({
   list: publicProcedure
@@ -43,38 +43,27 @@ const contactApi = createTRPCRouter({
       .returning()
       .then(c => asNonNil(c[0]))
 
-    const newNumbers = await eva(async () => {
-      const newPhoneNumbersInput = input.phoneNumbers
-        ?.filter(n => !n.id)
-        .map(n => ({ value: n.value, label: n.label, contactId: contact.id }))
+    const newPhoneNumbersInput = input.phoneNumbers
+      ?.filter(n => !n.id)
+      .map(n => ({ value: n.value, label: n.label, contactId: contact.id }))
 
-      if (!newPhoneNumbersInput || !newPhoneNumbersInput.length) return
+    if (newPhoneNumbersInput && newPhoneNumbersInput.length) {
+      await ctx.db.insert(PhoneNumber).values(newPhoneNumbersInput)
+    }
 
-      const numbers = await ctx.db.insert(PhoneNumber).values(newPhoneNumbersInput).returning()
-      return numbers
-    })
+    const existingPhoneNumbersInput = input.phoneNumbers
+      ?.filter(n => n.id)
+      .map(n => ({ ...n, contactId: contact.id }))
 
-    const existingNumbersUpdated = await eva(async () => {
-      const newPhoneNumbersInput = input.phoneNumbers
-        ?.filter(n => n.id)
-        .map(n => ({ ...n, contactId: contact.id }))
+    if (existingPhoneNumbersInput && existingPhoneNumbersInput.length) {
+      await Promise.all(
+        existingPhoneNumbersInput.map(number =>
+          ctx.db.update(PhoneNumber).set(number).where(eq(PhoneNumber.id, number.id)),
+        ),
+      )
+    }
 
-      if (!newPhoneNumbersInput || !newPhoneNumbersInput.length) return
-
-      const numbers = (
-        await Promise.all(
-          newPhoneNumbersInput.map(number =>
-            ctx.db.update(PhoneNumber).set(number).where(eq(PhoneNumber.id, number.id)).returning(),
-          ),
-        )
-      ).flat()
-
-      return numbers
-    })
-
-    const phoneNumbers = arrayWithoutNils([newNumbers, existingNumbersUpdated]).flat()
-
-    return { ...contact, phoneNumbers }
+    return contact
   }),
 
   create: publicProcedure.input(ContactCreateInput).mutation(({ ctx, input }) =>
