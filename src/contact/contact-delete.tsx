8aac2c18fc2d00def8b0c9eead7e1e1a8aa84e75
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-20 10:13:51 +0200

    contact delete update

--- src/contact/contact-delete.tsx
+++ src/contact/contact-delete.tsx
@@ -5,22 +5,20 @@ type Props = {
   contactId: number
   withHoverEffect?: boolean
   withText?: boolean
-  onClick?(): void
   onComplete?(): void
 }
 
 export function ContactDeleteAction({ contactId, onComplete, ...props }: Props) {
-  const { mutate } = api.contact.delete.useMutation()
-
-  async function handleConfirm() {
-    mutate(contactId)
-    onComplete && onComplete()
-  }
+  const { mutate } = api.contact.delete.useMutation({
+    onSuccess() {
+      onComplete && onComplete()
+    },
+  })
 
   return (
     <DeleteAction
       dialogText="Are you sure you want to delete this contact?"
-      onConfirm={handleConfirm}
+      onConfirm={() => mutate(contactId)}
       {...props}
     />
   )
