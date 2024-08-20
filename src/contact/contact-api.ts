c0e0a37b4e80785286747bb39723a523dceefcad
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-20 11:33:22 +0200

    contact api inputs update 4

--- src/contact/contact-api.ts
+++ src/contact/contact-api.ts
@@ -2,7 +2,7 @@ import { Contact, PhoneNumber } from "./contact-schema"
 import { createTRPCRouter, publicProcedure } from "~/api/trpc"
 import { z } from "zod"
 import { eq, desc, and, ilike, or, SQL } from "drizzle-orm"
-import { ContactFormInput } from "./contact-api-inputs"
+import { ContactFormInput } from "./contact-api-shared"
 import { getRandomAvatarStyle } from "./contact-data-generators"
 import { asNonNil, pick } from "~/utils"
 
