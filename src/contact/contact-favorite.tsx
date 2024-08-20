e73b4ec991d28271aae058933f0141c077b334c7
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-20 10:18:17 +0200

    contact favorite update

--- src/contact/contact-favorite.tsx
+++ src/contact/contact-favorite.tsx
@@ -1,42 +1,22 @@
 import { MouseEvent } from "react"
 import { HeartBorderIcon, HeartFilledIcon } from "~/assets/icons"
-import { updateMatches } from "~/utils"
 import { api } from "~/utils/api"
 
-function useContactUpdateOptimistic() {
-  const utils = api.useUtils()
-
-  return api.contact.update1.useMutation({
-    async onMutate(variables) {
-      await Promise.all([utils.contact.list.cancel(), utils.contact.single.cancel()])
-
-      utils.contact.list.setData(undefined, old => {
-        if (!old) return old
-        return updateMatches(
-          old,
-          c => c.id === variables.id,
-          c => ({ ...c, ...variables }),
-        )
-      })
-
-      utils.contact.single.setData(variables.id, old => old && { ...old, ...variables })
-    },
-
-    async onSettled(data, error, variables, context) {
-      await Promise.all([
-        utils.contact.single.invalidate(variables.id),
-        utils.contact.list.invalidate(),
-      ])
-    },
-  })
-}
 type ContactFavoriteProps = {
   id: number
   isFavorite: boolean
 }
 
 export function ContactFavorite({ id, isFavorite }: ContactFavoriteProps) {
-  const { mutate } = useContactUpdateOptimistic()
+  const apiUtils = api.useUtils()
+  const { mutate } = api.contact.update1.useMutation({
+    async onSettled(data, error, variables) {
+      await Promise.all([
+        apiUtils.contact.single.invalidate(variables.id),
+        apiUtils.contact.list.invalidate(),
+      ])
+    },
+  })
 
   function handleClick(e: MouseEvent) {
     e.stopPropagation()
