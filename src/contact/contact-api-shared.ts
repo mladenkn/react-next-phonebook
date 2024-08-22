bdd3fcc87ab1e095664de5c41e69715b4e6dd3df
Mladen Knezovic <mladen.knezovic.1993@gmail.com>
2024-08-22 20:49:29 +0200

    form validation 2

--- src/contact/contact-api-shared.ts
+++ src/contact/contact-api-shared.ts
@@ -1,14 +1,14 @@
 import { z } from "zod"
 
 export const ContactFormInput = z.object({
-  fullName: z.string().min(1, { message: "Potreban je unos" }),
+  fullName: z.string().min(1, { message: "Required" }),
   avatarUrl: z.string().url().nullish(),
-  email: z.string().email({ message: "Neispravan mail" }).optional(),
+  email: z.string().email({ message: "Invalid e-mail" }),
   phoneNumbers: z
     .array(
       z.object({
         id: z.number().optional(),
-        value: z.string().min(1, { message: "Potreban je unos" }),
+        value: z.string().min(1, { message: "Required" }),
         label: z.string(),
       }),
     )
